8581a3c73a128591877849eddc8b1cb01695a6f3
Kevincho9898 <47290520+Kevincho9898@users.noreply.github.com>
2019-03-03 01:27:17 -0800

    Create circle-grid.js

---
+++ circle-grid.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+	 for (var i = 20; i <= 400; i = i + 26){
+		 x=20
+	while(x<width){
+		ellipse(x,i,15,15)
+		x=x+20;
+	}
+
+	}
+}
